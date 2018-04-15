b652f52c7f59caa4694dd85bca36ae168d0ae368
Mark Kovacs <drmarkaron@gmail.com>
2018-04-15 15:54:09 +0200

    (Spaceship App) planet class created, with getters-setters

--- src/main/java/com/greenfoxacademy/spaceship/model/Planet.java
+++ src/main/java/com/greenfoxacademy/spaceship/model/Planet.java
@@ -1,4 +1,35 @@
 package com.greenfoxacademy.spaceship.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Planet {
+
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
+  private Long id;
+  private String name;
+  private int population;
+
+  public Planet() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPopulation() {
+    return population;
+  }
+
+  public void setPopulation(int population) {
+    this.population = population;
+  }
 }
